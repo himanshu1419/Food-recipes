f6fffa8c456f06a890b5bc17afb3d408643ee607
himanshunegi1419 <himanshunegi1419@gmail.com>
2024-12-26 15:36:08 +0530

    creating React components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Home from "./pages/home";
+import Favorites from "./pages/favorites";
+import Details from "./pages/details";
 
 function App() {
-
-
   return (
-    
-      <div>
-        
+    <div>
+      <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/recipe-item/:id" element={<Details />} />
+        </Routes>
       </div>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
