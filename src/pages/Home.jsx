c8c8ddc7f71bd09b98893adbc16a2aac5a518e94
himanshunegi1419 <himanshunegi1419@gmail.com>
2024-12-31 16:33:55 +0530

    Adding Home section to display searched items

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { useContext } from "react";
+import { GlobalContext } from "../context/index";
+import RecipeItem from "../components/RecipeItem";
 
-const Home = () => {
+export default function Home() {
+  const { recipeList, loading } = useContext(GlobalContext);
+
+  if (loading) return <div>Loading...Please wait!</div>;
+
   return (
-    <div>Home</div>
-  )
+    <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+      {recipeList && recipeList.length > 0 ? (
+        recipeList.map((item) => <RecipeItem item={item} />)
+      ) : (
+        <div>
+          <p className="lg:text-4xl text-xl text-center text-black font-extrabold">
+            Nothing to show. Please search something
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
-
-export default Home
